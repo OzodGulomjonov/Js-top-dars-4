0edf61da4a686df04c9554d0ac6cdfb8afd8a34e
OzodGulomjonov <102744037+OzodGulomjonov@users.noreply.github.com>
2022-07-03 23:44:51 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+alert ("Salom iltimos malumotlaringizni kiriting !");
+
+var userName = prompt ("iltimos ismingizni kiriting !" , "Falonchi");
+var userSurname = prompt ("Iltimos familiyangizni kiriting !" , "pismadonchiev");
+var userFatherName = prompt ("iltimos ochestvangizni kiriting" , "Falonchi ogli");
+var userAge = prompt ("Iltimos yoshingizni kiriting !");
+var userJob = prompt ("Iltimos nima ish qilishingizni kiriting !");
+var userHobby = prompt ("iltimos hobbyingizni kiriting !"); 
+
+alert (`Ismingiz ${userName}\nfamiliyangiz ${userSurname}\nochestvangiz ${userFatherName}\nyoshingiz ${userAge} da\n 
+ishingiz ${userJob}\nhobbyingiz ${userHobby} `);
+
+console.log (`Ismingiz ${userName}\nfamiliyangiz ${userSurname}\nochestvangiz ${userFatherName}\nyoshingiz ${userAge}da\nishingiz ${userJob}\nhobbyingiz ${userHobby}`);
